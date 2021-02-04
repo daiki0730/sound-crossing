715a403d3ceb11eb3a92a7b9153fe567658f8125
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-02-04 23:18:07 +0900

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'last_name'       => '西田',
+            'first_name'      => '大輝',
+            'last_name_kana'  => 'ニシダ',
+            'first_name_kana' => 'ダイキ',
+            'email'           => 'daikibassman@gmail.com',
+            'password'        => Hash::make('secret'),
+        ]);
     }
 }
