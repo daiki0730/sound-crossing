ed83004e7ffbaa110695f492a02d1e9f092ea49c
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-02-11 16:42:25 +0900

    create adminlte view

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'      => '',
-            'name_kana' => '',
-            'email'     => 'z41.daiki.nishida@gmail.com',
+            'name'      => 'てすと',
+            'name_kana' => 'テスト',
+            'email'     => 'test@test.com',
             'password'  => Hash::make('secret'),
             'gender'    => 1,
             'birthdate' => '1991-07-30',
