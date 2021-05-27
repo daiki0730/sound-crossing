605bcb280e14c59c7a1abf0e9eabf32e930cce16
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-05-27 23:57:18 +0900

    fix

--- resources/views/backend/admin/user/index.blade.php
+++ resources/views/backend/admin/user/index.blade.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>ユーザー一覧</h1>
+                <h1>ユーザ一覧</h1>
             </div>
             <div class="col-sm-2 ml-auto">
                 <a href="{{ route('backend.admin.users.create') }}" type="button" class="btn btn-block btn-secondary float-right">
