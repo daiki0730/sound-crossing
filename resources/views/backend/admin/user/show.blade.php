be1d29bc27ce98226b3053a08a0db840f740201c
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-06-12 16:35:49 +0900

    viewを作成

--- resources/views/backend/admin/user/show.blade.php
+++ resources/views/backend/admin/user/show.blade.php
@@ -32,8 +32,7 @@
                     <div class="card-header bg-navy text-white">
                         <h3 class="card-title">ユーザー一覧</h3>
                     </div>
-
-                    <form action="{{ route('backend.admin.users.store') }}" method="post">
+                    <form action="{{ route('backend.admin.users.edit') }}" method="post">
                         @csrf
                         <input name="status" type="hidden" value="1">
                         <div class="card-body">
@@ -41,7 +40,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <label>ニックネーム <span class="text-danger">※</span></label>
-                                        <input type="text" name="name" class="form-control @error("name") is-invalid @enderror" placeholder="ニックネーム">
+                                        <input type="text" name="name" class="form-control @error("name") is-invalid @enderror" value="{{ old('name') ?: $user->name }}">
                                         @error("name")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("name") }}</strong>
@@ -50,7 +49,7 @@
                                     </div>
                                     <div class="form-group">
                                         <label>ニックネーム(カナ) <span class="text-danger">※</span></label>
-                                        <input type="text" name="name_kana" class="form-control @error("name_kana") is-invalid @enderror" placeholder="ニックネーム(カナ)">
+                                        <input type="text" name="name_kana" class="form-control @error("name_kana") is-invalid @enderror" value="{{ old('name_kana') ?: $user->name_kana }}">
                                         @error("name_kana")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("name_kana") }}</strong>
@@ -59,7 +58,7 @@
                                     </div>
                                     <div class="form-group">
                                         <label>メールアドレス <span class="text-danger">※</span></label>
-                                        <input type="email" name="email" class="form-control @error("email") is-invalid @enderror" placeholder="メールアドレス">
+                                        <input type="text" name="email" class="form-control @error("email") is-invalid @enderror" value="{{ old('email') ?: $user->email }}">
                                         @error("email")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("email") }}</strong>
@@ -67,8 +66,8 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label>性別</label>
-                                        <input type="text" name="gender" class="form-control @error("gender") is-invalid @enderror" placeholder="性別">
+                                        <label>性別 <span class="text-danger">※</span></label>
+                                        <input type="text" name="gender" class="form-control @error("gender") is-invalid @enderror" value="{{ old('gender') ?: $user->gender }}">
                                         @error("gender")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("gender") }}</strong>
@@ -76,8 +75,8 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label>生年月日</label>
-                                        <input type="datetime-local" name="birthdate" class="form-control @error("birthdate") is-invalid @enderror" placeholder="生年月日">
+                                        <label>生年月日 <span class="text-danger">※</span></label>
+                                        <input type="text" name="birthdate" class="form-control @error("birthdate") is-invalid @enderror" value="{{ old('birthdate') ?: $user->birthdate }}">
                                         @error("birthdate")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("birthdate") }}</strong>
@@ -85,8 +84,8 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label>パスワード <span class="text-danger">※</span></label>
-                                        <input type="password" name="password" class="form-control @error("password") is-invalid @enderror" placeholder="パスワード">
+                                        <label>パスワード</label>
+                                        <input type="text" name="password" class="form-control @error("password") is-invalid @enderror">
                                         @error("password")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("password") }}</strong>
@@ -94,8 +93,8 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label>パスワード(確認用) <span class="text-danger">※</span></label>
-                                        <input type="password" name="password_confirmation" class="form-control @error("password_confirmation") is-invalid @enderror" placeholder="パスワード">
+                                        <label>パスワード(確認用)</label>
+                                        <input type="text" name="password_confirmation" class="form-control @error("password_confirmation") is-invalid @enderror">
                                         @error("password_confirmation")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("password_confirmation") }}</strong>
@@ -105,8 +104,8 @@
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label>自己紹介文</label>
-                                        <textarea name="biography" class="form-control @error("") is-invalid @enderror" rows="1" placeholder="自己紹介文"></textarea>
+                                        <label>自己紹介文 <span class="text-danger">※</span></label>
+                                        <input type="text" name="biography" class="form-control @error("biography") is-invalid @enderror" value="{{ old('biography') ?: $user->biography }}">
                                         @error("biography")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("biography") }}</strong>
@@ -114,8 +113,8 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label>自己紹介URL</label>
-                                        <input type="text" name="url" class="form-control @error("") is-invalid @enderror" placeholder="URL">
+                                        <label>自己紹介URL <span class="text-danger">※</span></label>
+                                        <input type="text" name="url" class="form-control @error("url") is-invalid @enderror" value="{{ old('url') ?: $user->url }}">
                                         @error("url")
                                         <div class="invalid-feedback">
                                             <strong>{{ $errors->first("url") }}</strong>
@@ -136,7 +135,7 @@
                         </div>
                         <div class="card-footer" id="date-generate">
                             <a href="{{ route('backend.admin.users.index') }}" class="btn btn-secondary float-right">キャンセル</a>
-                            <button type="submit" class="btn btn-primary float-right " style="margin-right: 10px">新規登録</button>
+                            <button type="submit" class="btn btn-primary float-right " style="margin-right: 10px">更新</button>
                         </div>
                     </form>
                 </div>
