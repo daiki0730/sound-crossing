fe7ba1bf405b6736bfa9f2159bf503cf84b8e487
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-08-17 22:52:55 +0900

    クラスのインポート

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -3,6 +3,9 @@
 /**
  * backend routes
  */
+
+use Illuminate\Routing\Route;
+
 Route::namespace('Backend')->prefix('admin')->name('backend.admin.')->group(function () {
   Route::resource('users', 'UserController');
 });
