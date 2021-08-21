17db7464fa9a5221bc606654499b254dd4e7488f
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-08-21 23:47:29 +0900

    Update backend.php

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -4,6 +4,7 @@
  * backend routes
  */
 
+
 use Illuminate\Routing\Route;
 Route::namespace('Backend')->prefix('admin')->name('backend.admin.')->group(function () {
   Route::resource('users', 'UserController');
