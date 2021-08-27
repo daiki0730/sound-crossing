5a529bbefe0fb9937d41a558917cb3fcfcdcb461
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-08-27 22:02:06 +0900

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,18 @@ class AppServiceProvider extends ServiceProvider
     ];
 
     /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        foreach ($this->bindings as $abstruct => $concrete) {
+            $this->app->bind($abstruct, $concrete);
+        }
+    }
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
