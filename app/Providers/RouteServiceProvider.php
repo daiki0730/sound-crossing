949f82d40d98eaf5c355578046f5cc7dd7a58c2a
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-08-31 23:42:37 +0900

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -76,7 +76,6 @@ class RouteServiceProvider extends ServiceProvider
             ->namespace($this->namespace)
             ->group(base_path('routes/backend/backend.php'));
     }
-    
 
 
     /**
