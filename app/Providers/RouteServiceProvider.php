b20c078dee979e8bd8db90fac2547dfde0cfaae6
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-09-20 23:39:46 +0900

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -60,7 +60,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
              ->namespace($this->namespace)
-             ->group(base_path('routes/frontend.php'));
+             ->group(base_path('routes/frontend/frontend.php'));
     }
 
     /**
@@ -74,7 +74,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
             ->namespace($this->namespace)
-            ->group(base_path('routes/backend.php'));
+            ->group(base_path('routes/backend/backend.php'));
     }
 
     /**
