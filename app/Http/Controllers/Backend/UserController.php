16ce147fbb96b0284af5231411c6875893773661
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-10-10 23:48:18 +0900

    Update UserController.php

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\User\UserServiceInterface;
 use Illuminate\Support\Facades\Log;
+use App\Http\Requests\Backend\CreateUserRequest;
 
 
 class UserController extends Controller
