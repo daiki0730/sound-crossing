8ccde51287109946cb5ef30658d3be148a4940de
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-11-14 23:52:55 +0900

    不要な行を削除

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -4,7 +4,6 @@
  * backend routes
  */
 
-
 use Illuminate\Support\Facades\Route;
 Route::namespace('Backend')->prefix('admin')->name('backend.admin.')->group(function () {
   Route::resource('users', 'UserController');
